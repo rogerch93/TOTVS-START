32fd41b25db1d67a39d40bcc4f6f1db988cad193
rogerch93 <rogerinhochaves@hotmail.com>
2021-12-19 17:59:49 -0300

    adc animacao no btn download

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 const target = document.querySelectorAll('[data-animation]');
 const animationClass = 'animation';
+const animationClassImg = 'animationImg';
+const animationClassDownload = 'animationDownload';
 
 function animeScroll(){
-  const windowTop = window.scrollY+1;
+  const windowTop = window.screenY++;
   target.forEach(function(elt){
     if((windowTop) > elt.scrollTop){
       elt.classList.add(animationClass)
+      elt.classList.add(animationClassDownload);
     }
   })
 }
 
-window.addEventListener('scroll', function(){
+window.addEventListener('mouseout', function(){
   animeScroll();
 })
