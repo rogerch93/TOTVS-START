e156ad6bf9183b801ce1536c7b0d01fd58f918d9
rogerch93 <rogerinhochaves@hotmail.com>
2021-12-19 21:42:41 -0300

    ajustar animação works

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
+/*Animação do topo */
 const target = document.querySelectorAll('[data-animation],[data-animationDownload],[data-animationImg]');
 const animationClass = 'animation';
 const animationClassImg = 'animationImg';
 const animationClassDownload = 'animationD';
 
-function animeScroll(){
+function animeMouse(){
   const windowTop = window.screenY++;
   target.forEach(function(elt){
     if((windowTop) > elt.scrollTop){
@@ -15,5 +16,22 @@ function animeScroll(){
 }
 
 window.addEventListener('mouseout', function(){
+  animeMouse();
+})
+
+/*Animação scroll */
+const workTarget = document.querySelectorAll('[data-animationImgWorks]');
+const animationClassWorks = 'animationImgWorks';
+
+function animeScroll(){
+  const windowScroll = window.scrollY;
+  workTarget.forEach(function(elt){
+    if((windowScroll) > elt.scrollTop){
+      elt.classList.add(animationClassWorks)
+    }
+  })
+}
+
+window.addEventListener('scroll', function(){
   animeScroll();
 })
